d605e20ae0d5048328ab434517536323a0f5cd04
geh4y8 <guy.halperin88@gmail.com>
2014-11-19 01:40:21 -0800

    add visibility

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('header').addClass('visibility');
+	$('.carousel-iphone').addClass('visibility');
+	$('.payoff h1').addClass('visibility');
+	$('.features .col-md-4').addClass('visibility');
+	$('.social .col-md-12').addClass('visibility');
+});
