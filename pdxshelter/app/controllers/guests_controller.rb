cc6d75c9cdf5510a738902c0351bf3f891271854
geh4y8 <guy.halperin88@gmail.com>
2014-12-01 14:29:01 -0600

    add guest model

--- pdxshelter/app/controllers/guests_controller.rb
+++ pdxshelter/app/controllers/guests_controller.rb
@@ -1,9 +1,47 @@
 class GuestsController < ApplicationController
 
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!
 
   def index
-
+    @guests = Guest.all
   end
 
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    @guest = Guest.new(params[:guest])
+    if @guest.save
+      flash[:notice] = "Guest created."
+      redirect_to guests_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @guest = Guest.find(params[:id])
+  end
+
+  def edit
+    @guest = Guest.find(params[:id])
+  end
+
+  def update
+    @guest = Guest.find(params[:id])
+    if @guest.update(params[:guest])
+      flash[:notice] = "Guest information updated"
+      redirect_to guest_path(@guest)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @guest = Guest.find(params[:id])
+    @guest.destroy
+    flash[:notice] = "Guest deleted from system"
+    redirect_to guests_path
+  end
 end
---
+++ pdxshelter/app/models/guest.rb
@@ -0,0 +1,3 @@
+class Guest < ActiveRecord::Base
+  
+end
